405308f203e25d02e4c68cc647b55324681958c7
Purukitto <purukitto@gmail.com>
2023-07-24 19:03:45 +0530

    Working on server

---
+++ models/File.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const File = new mongoose.Schema({
+	path: { type: String, required: true },
+	originalName: { type: String, required: true },
+	password: String,
+	downloadCount: { type: Number, default: 0, required: true },
+});
+
+module.exports = mongoose.model("File", File);
