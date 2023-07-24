17535284246c6e457c7b2e885b23738aa61adfa8
Purukitto <purukitto@gmail.com>
2023-07-25 03:54:13 +0530

    Base UI done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,8 @@
-import { PhotoIcon } from "@heroicons/react/24/solid";
+import {
+	PhotoIcon,
+	ClipboardDocumentIcon,
+	FolderArrowDownIcon,
+} from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 export default function App() {
@@ -56,18 +60,53 @@ export default function App() {
 	};
 
 	return (
-		<div className="bg-slate-800 flex items-center justify-center py-10">
+		<div className="bg-slate-800 flex items-center justify-center h-[100vh]">
 			<form>
 				<div className="space-y-12">
 					<div className="border-b border-white/10 pb-12">
-						<h2 className="text-base font-semibold leading-7 text-white ">
+						<h2 className="font-semibold leading-7 text-xl text-white ">
 							FileShare
 						</h2>
 						<p className="mt-1 text-sm leading-6 text-gray-400">
 							A simple file sharing demo app
 						</p>
 
-						<div className=" py-6">{link ? link : null}</div>
+						<div className=" py-6">
+							{link ? (
+								<div className="flex gap-x-6">
+									<button
+										type="button"
+										onClick={() => {
+											navigator.clipboard.writeText(
+												link.props.href
+											);
+										}}
+										className="inline-flex items-center gap-x-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>
+										Copy download link
+										<ClipboardDocumentIcon
+											className="-mr-0.5 h-5 w-5"
+											aria-hidden="true"
+										/>
+									</button>
+									<button
+										type="button"
+										onClick={() => {
+											navigator.clipboard.writeText(
+												link.props.href
+											);
+										}}
+										className="inline-flex items-center gap-x-2 rounded-md px-3.5 py-2.5 text-sm font-semibold bg-white/10 text-white shadow-sm hover:bg-white/20"
+									>
+										Download File
+										<FolderArrowDownIcon
+											className="-mr-0.5 h-5 w-5"
+											aria-hidden="true"
+										/>
+									</button>
+								</div>
+							) : null}
+						</div>
 
 						<div className="col-span-full">
 							<div className="block text-sm font-medium leading-6 text-white">
