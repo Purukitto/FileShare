827b9858cda4ee4b949f011a0f815335d327aa4f
Purukitto <purukitto@gmail.com>
2023-07-26 16:07:04 +0530

    File model comments

--- Backend/models/File.js
+++ Backend/models/File.js
@@ -1,10 +1,20 @@
+// Import the 'mongoose' library
 const mongoose = require("mongoose");
 
+// Define the 'File' schema
 const File = new mongoose.Schema({
+	// 'path' represents the file's storage location, it is a required field
 	path: { type: String, required: true },
+
+	// 'originalName' holds the original name of the uploaded file, required field
 	originalName: { type: String, required: true },
+
+	// 'password' stores the hashed password for password-protected files, optional field
 	password: String,
+
+	// 'downloadCount' keeps track of the number of times the file has been downloaded, default is 0
 	downloadCount: { type: Number, default: 0, required: true },
 });
 
+// Create and export the 'File' model
 module.exports = mongoose.model("File", File);
